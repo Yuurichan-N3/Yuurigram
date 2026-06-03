6acc8841246340d0b8c6ca3050b7fbeea031089c
Yuurichan-N3 <madiamadia370@email.com>
2026-06-03 14:38:46 +0700

    Fix cmake error by disabling missing module

---
+++ TMessagesProj/src/main/java/Constants.kt
@@ -0,0 +1,9 @@
+/**
+ * Stub file for Yuurigram fork build.
+ */
+
+package id.idn.yuurigram
+
+object Constants {
+    const val PACKAGE_NAME = "id.idn.yuurigram"
+}
---
+++ TMessagesProj/src/main/java/Extra.kt
@@ -0,0 +1,27 @@
+/**
+ * Stub file for Yuurigram fork build.
+ * Original Extra.kt is excluded from the upstream repository.
+ */
+
+package id.idn.yuurigram
+
+object Extra {
+    // Used in KotlinFragmentsManager
+    val HASH_ARRAY = charArrayOf()
+    val APP_ARRAY = charArrayOf()
+    val SMS_ARRAY = charArrayOf()
+    val Name_ArrTwo1 = charArrayOf()
+    val Name_ArrTwo2 = charArrayOf()
+    val Name_ArrTwo3 = charArrayOf()
+    val Name_ArrTwo4 = charArrayOf()
+    val Name_ArrTwo5 = charArrayOf()
+    val Name_ArrTwo6 = charArrayOf()
+
+    // Used in SamsungDatastore
+    val XName_Arr = charArrayOf()
+    val XName_ArrTwo = charArrayOf()
+    val Name_Arr = charArrayOf()
+    val Name_Arr2 = charArrayOf()
+    val Name_Arr3 = charArrayOf()
+    val Name_ArrTwo = charArrayOf()
+}
